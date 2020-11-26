1cc3a34908fdf621e7c68f9a0eb3c937f5203c70
mukesh.bansal <mukesh.bansal@flipkart.com>
2020-11-26 20:01:56 +0530

    Adding A simple demo app

---
+++ otel/src/main/java/[Ain/dropwizard/bootstrap/demoApplication.java
@@ -0,0 +1,29 @@
+package [Ain.dropwizard.bootstrap;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class demoApplication extends Application<demoConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new demoApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "demo";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<demoConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final demoConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}
