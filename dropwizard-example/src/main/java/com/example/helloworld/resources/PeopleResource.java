d9c1f6309d8602af5c16d264b70f0e5e46292260
mukesh.bansal <mukesh.bansal@flipkart.com>
2020-11-26 12:14:34 +0530

    ADding Trace Context Fetcher

--- dropwizard-example/src/main/java/com/example/helloworld/resources/PeopleResource.java
+++ dropwizard-example/src/main/java/com/example/helloworld/resources/PeopleResource.java
@@ -3,13 +3,17 @@ package com.example.helloworld.resources;
 import com.example.helloworld.core.Person;
 import com.example.helloworld.db.PersonDAO;
 import io.dropwizard.hibernate.UnitOfWork;
+import io.opentelemetry.api.*;
+import io.opentelemetry.api.trace.*;
+import io.opentelemetry.context.propagation.*;
 
+import javax.servlet.http.*;
 import javax.validation.Valid;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.*;
 import java.util.List;
 
 @Path("/people")
@@ -18,6 +22,24 @@ public class PeopleResource {
 
     private final PersonDAO peopleDAO;
 
+    @Context
+    private HttpServletRequest httpRequest;
+
+    TracerProvider tracerProvider = OpenTelemetry.getGlobalTracerProvider();
+    Tracer tracer = tracerProvider.get("instrumentation-library-name","1.0.0");
+    TextMapPropagator.Getter<HttpServletRequest> getter =
+            new TextMapPropagator.Getter<HttpServletRequest>() {
+                @Override
+                public Iterable<String> keys(HttpServletRequest httpServletRequest) {
+                    return null;
+                }
+
+                @Override
+                public String get(HttpServletRequest carrier, String key) {
+                    return carrier.getHeader(key);
+                }
+            };
+
     public PeopleResource(PersonDAO peopleDAO) {
         this.peopleDAO = peopleDAO;
     }
@@ -25,6 +47,12 @@ public class PeopleResource {
     @POST
     @UnitOfWork
     public Person createPerson(@Valid Person person) {
+        io.opentelemetry.context.Context extractedContext = OpenTelemetry.getGlobalPropagators().getTextMapPropagator().extract(
+                io.opentelemetry.context.Context.current(),
+                httpRequest,
+                getter);
+        // Push To Kafka
+        // A Separate Microservice Consumes from Kafka <-- How to connect the trace for that microservice ??
         return peopleDAO.create(person);
     }
 
@@ -33,5 +61,7 @@ public class PeopleResource {
     public List<Person> listPeople() {
         return peopleDAO.findAll();
     }
+
+
 
 }
