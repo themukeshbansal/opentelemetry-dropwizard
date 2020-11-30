a027cd38b8b38f5086e7a0726abde17c5b379988
mukesh.bansal <mukesh.bansal@flipkart.com>
2020-11-30 15:40:43 +0530

    Fresh install

---
+++ otel/src/main/java/in/drop/wizard/demoConfiguration.java
@@ -0,0 +1,10 @@
+package in.drop.wizard;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class demoConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
