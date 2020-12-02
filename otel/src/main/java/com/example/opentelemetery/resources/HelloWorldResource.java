f2e2b5a3c09b97c54c43046d2d34f1a2bf7ab473
mukesh.bansal <mukesh.bansal@flipkart.com>
2020-12-02 12:59:14 +0530

    Test First Commit

--- otel/src/main/java/com/example/opentelemetery/resources/HelloWorldResource.java
+++ otel/src/main/java/com/example/opentelemetery/resources/HelloWorldResource.java
@@ -35,25 +35,25 @@ public class HelloWorldResource {
     public Saying sayHello(@QueryParam("name") Optional<String> name,
                            @Context HttpServletRequest httpRequest) {
 
-        TracerProvider tracerProvider = OpenTelemetry.getGlobalTracerProvider();
-        Tracer tracer = tracerProvider.get("instrumentation-library-name","1.0.0");
-        TextMapPropagator.Getter<HttpServletRequest> getter =
-                new TextMapPropagator.Getter<HttpServletRequest>() {
-                    @Override
-                    public Iterable<String> keys(HttpServletRequest httpServletRequest) {
-                        return null;
-                    }
-
-                    @Override
-                    public String get(HttpServletRequest carrier, String key) {
-                        return carrier.getHeader(key);
-                    }
-                };
-
-        io.opentelemetry.context.Context extractedContext = OpenTelemetry.getGlobalPropagators().getTextMapPropagator().extract(
-                io.opentelemetry.context.Context.current(),
-                httpRequest,
-                getter);
+//        TracerProvider tracerProvider = OpenTelemetry.getGlobalTracerProvider();
+//        Tracer tracer = tracerProvider.get("instrumentation-library-name","1.0.0");
+//        TextMapPropagator.Getter<HttpServletRequest> getter =
+//                new TextMapPropagator.Getter<HttpServletRequest>() {
+//                    @Override
+//                    public Iterable<String> keys(HttpServletRequest httpServletRequest) {
+//                        return null;
+//                    }
+//
+//                    @Override
+//                    public String get(HttpServletRequest carrier, String key) {
+//                        return carrier.getHeader(key);
+//                    }
+//                };
+//
+//        io.opentelemetry.context.Context extractedContext = OpenTelemetry.getGlobalPropagators().getTextMapPropagator().extract(
+//                io.opentelemetry.context.Context.current(),
+//                httpRequest,
+//                getter);
         final String value = String.format(template, name.orElse(defaultName));
         return new Saying(counter.incrementAndGet(), value);
     }
